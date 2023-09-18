18a223a02f49dfc5e27649da55c6825d68fa03bd
TA�ARA FERNANDA SANCHES DIAS <tayara.5970@aluno.pr.senac.br>
2023-09-18 15:55:40 -0300

    primeiro commit

---
+++ src/main/java/com/mycompany/pessoas/Pessoas.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pessoas;
+
+/**
+ *
+ * @author tayara.5970
+ */
+public class Pessoas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.print("nome, idade,");
+        
+        System.out.println("25");
+        
+        
+        
+        
+        
+    
+        
+                
+                
+        System.out.print("Qual o nome dela?");
+       
+        
+        System.out.println("Qual sua idade?");
+                
+                        
+            
+        
+        
+        
+       
+       
+       
+            
+                
+    
+    }
+}
